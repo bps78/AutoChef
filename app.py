862f8f6f51de37293bf86331e17086e1c3ce2e28
bps78 <90796928+bps78@users.noreply.github.com>
2025-08-10 14:43:01 -0500

    Alternate fix for file watcher

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import requests
 import torch
 from ultralytics.nn.tasks import DetectionModel
 
-st.set_option("server.fileWatcherType", "none") # Disable file watcher to prevent issues with large models
-
 # Allow this class to be unpickled
 torch.serialization.add_safe_globals([DetectionModel])
 
